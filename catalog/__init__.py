e923c73b3e361ea35595e511c7fcf1b7f6b8f343
David A Negrete <danegrete79@gmail.com>
2016-01-18 19:04:01 -0700

    Changing __init__ file

--- catalog/__init__.py
+++ catalog/__init__.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
+from flask import flash
+from flask import jsonify
+from sqlalchemy import create_engine, and_
+from sqlalchemy.orm import sessionmaker
+
 app = Flask(__name__)
+
+
 @app.route("/")
+
 def hello():
     return "Hello, I love Digital Ocean!"
 if __name__ == "__main__":
